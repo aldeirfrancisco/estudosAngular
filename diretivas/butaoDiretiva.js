5e9edc94528cab0f561b9877cf2334b03697464b
aldeirfrancisco <dide123francisco@gmail.com>
2021-02-27 23:37:33 -0300

    refatorando o codigo

--- diretivas/butaoDiretiva.js
+++ diretivas/butaoDiretiva.js
@@ -4,10 +4,14 @@ angular.module('app').directive('btnButao', function() {
         restrict: 'E',
         templateUrl:  'html/butao.html',
        scope:{
-           disabiltar:"=disabiltar",
-           btnNome:"=botaoNome",
-           acao :"&acao",
+           disabiltar:"=",
+           btnNome:"=",
+           acao :"&",
        }
     };
   });
-  /*btnnome e o nome da variavel que vai na div do butao eo botaoNome vai ser usando com atributo na diretiva */
+  /*btnnome e o nome da variavel que vai na div do butao eo botaoNome 
+  vai ser usando com atributo na diretiva 
+   = vai fazer um two-way dataBinding ou seja vai altera no controle e na div
+   & aqui ta fazendo uma execução 
+  */

dad44b7f050f7cabcd2afe483d235e8fe9fb1c25
aldeirfrancisco <dide123francisco@gmail.com>
2021-04-04 11:55:33 -0300

    refatorando o butao

--- app/controllers/controlePedido.js
+++ app/controllers/controlePedido.js
@@ -1,12 +1,22 @@
 angular.module('app').controller('controlePedido',['$scope',function($scope,){
 $scope.mostrarEndereco =false;
 $scope.finalizar="Finalizar";
-
 $scope.enderecoModal = function(){
     $scope.mostrarEndereco =true;
 }
 $scope.cadastrarEndereco = function(){
     $scope.mostrarEndereco =false;
 }
+ const finalizar = function(){
+      
+ }
 
+  
+$scope.acoesPedido = {
+         filalizar:{
+               nome:"Finalizar",
+               acao:finalizar,
+               style:"styleButao",
+         }
+}
 }]);

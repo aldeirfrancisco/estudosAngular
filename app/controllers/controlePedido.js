d2c1c160941ebd430b24e6aceaed9db2ad2c6272
aldeirfrancisco <dide123francisco@gmail.com>
2021-04-04 18:43:17 -0300

    criando o objeto pedido

--- app/controllers/controlePedido.js
+++ app/controllers/controlePedido.js
@@ -1,10 +1,14 @@
 angular.module('app').controller('controlePedido',['$scope',function($scope,){
-$scope.soNumero = /^\d+$/;
-$scope.mostrarEndereco =false;
-$scope.finalizar="Finalizar";
-$scope.pedidos=[];
+    $scope.soNumero = /^\d+$/;
+    $scope.mostrarEndereco =false;
+    $scope.campo=false;
+    $scope.finalizar="Finalizar";
+    $scope.pedidos=[];
+    $scope.produtos =[];
+  
 const enderecoModal = function(){
     $scope.mostrarEndereco =true;
+    
 }
 const finalizar = function(){
     return  console.log("aqui")
@@ -27,15 +31,47 @@ $scope.acoesPedido = {
         style:"styleButao3",
     },
 }
+
 $scope.cadastrarEndereco = function(cliente){
     $scope.mostrarEndereco =false;
     $scope.pedidos.push(cliente)
-    console.log(cliente)
 }
+  
 
-    
-    
+        $(document).keypress(function(e) {
+            let produto={};
+            if(e.which == 13){
+                
+                 let nome = document.getElementById("idnome").value;
+                 let codigo = document.getElementById("idcodigo").value;
+                 let quantidade = document.getElementById("idquantidade").value;
+                 let mesa = document.getElementById("idmesa").value;
+                 let obs = document.getElementById("idobs").value;
+                 if(nome.length >0 && codigo.length > 0 && quantidade.length > 0 && mesa.length >0&&  obs.length >0){
+                     document.getElementById("idnome").value="";
+                     document.getElementById("idcodigo").value="";
+                     document.getElementById("idquantidade").value="";
+                     document.getElementById("idobs").value="";
+                     document.getElementById("idmesa").value="";
+                     produto.nome = nome;
+                     produto.codigo = codigo;
+                     produto.quantidade = quantidade;
+                     produto.mesa = mesa;
+                     produto.observacao = obs;
+                     $scope.pedidos.push(produto)
+                     console.log(produto)
+                 }
+                 
 
- 
+
+               
+            }
+        });
+
+         const init = ()=>{
+            $scope.pedidos;
+         }
+  
+ init();
 
 }]);
